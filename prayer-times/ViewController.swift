be89734ffd07fa220af6175525c06617fe580e54
alkalemir <alkalemir@gmail.com>
2023-05-19 16:51:34 +0300

    Initial Commit

---
+++ prayer-times/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prayer-times
+//
+//  Created by Emir Alkal on 19.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
